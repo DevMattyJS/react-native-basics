d5c8ab7b5a779c84f8e102bfaea6d6efc25eaf87
DevMattyJS <matej.sliacky@protonmail.com>
2023-02-05 18:55:57 +0100

    Some styling added

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { StyleSheet, View, Button, TextInput, FlatList } from "react-native";
+import { StyleSheet, View, Button, FlatList } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
@@ -34,40 +35,43 @@ export default function App() {
   }
 
   return (
-    // View it's like a container element
-    <View style={styles.appContainer}>
-      <Button
-        title="Add New Goal"
-        color={"#5e0acc"}
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}
-        visible={modalIsVisible}
-      />
-      <View style={styles.goalsContainer}>
-        {/* FlatList component is scrollable, but just the currently visible items are rendered */}
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteGoalHandler}
-              />
-            );
-          }}
-          // If we loading our data from API and there is some property, which we can use as a good unique key
-          // we can use a keyExtractor prop to extract that key from a data and tell Flatlist to use it
-          // If we setting up our own data, it's better to set a 'key' property to our data and Flatlist will automatically use it
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
+    <>
+      <StatusBar style="light" />
+      {/* View it's like a container element */}
+      <View style={styles.appContainer}>
+        <Button
+          title="Add New Goal"
+          color="#a065ec"
+          onPress={startAddGoalHandler}
         />
+        <GoalInput
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+          visible={modalIsVisible}
+        />
+        <View style={styles.goalsContainer}>
+          {/* FlatList component is scrollable, but just the currently visible items are rendered */}
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            // If we loading our data from API and there is some property, which we can use as a good unique key
+            // we can use a keyExtractor prop to extract that key from a data and tell Flatlist to use it
+            // If we setting up our own data, it's better to set a 'key' property to our data and Flatlist will automatically use it
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -82,6 +86,9 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
+
+    //* we set a backgoundColor in app.json file, so we don't have to set it up in all screens separately
+    // backgroundColor: "#1e085a",
   },
 
   goalsContainer: {
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Image,
+} from "react-native";
 
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -20,6 +27,10 @@ function GoalInput(props) {
       statusBarTranslucent={true}
     >
       <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -30,10 +41,10 @@ function GoalInput(props) {
           {/* Build in Button component don't have a style property,*/}
           {/*  so if we want to style it we need to wrap it in a View or create our own Button component  */}
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} />
+            <Button title="Add Goal" onPress={addGoalHandler} color="#5e0acc" />
           </View>
         </View>
       </View>
@@ -48,18 +59,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    backgroundColor: "#311B6B",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
   //* A key feature to build layouts in a React Native apps is 'flexbox'
   //* every View by default use a flexbox and it is setup to organizing children components in a column
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    borderRadius: 6,
+    backgroundColor: "#e4d0ff",
+    color: "#120438",
     width: "100%",
-    padding: 8,
+    padding: 16,
   },
   buttonContainer: {
     marginTop: 16,
